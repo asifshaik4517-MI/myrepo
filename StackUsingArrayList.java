f37cd7d3830418269c4115a8ea0a079866b13046
asifshaik4517-MI <asifshaik4517@gmail.com>
2025-08-19 15:01:28 +0530

    Update StackUsingArrayList.java

--- StackUsingArrayList.java
+++ StackUsingArrayList.java
@@ -1,16 +1,12 @@
 import java.util.ArrayList;
-
 public class StackUsingArrayList {
     private ArrayList<Integer> elements;
-
     public StackUsingArrayList() {
         elements = new ArrayList<>();
     }
-
     public void push(int value) {
         elements.add(value);
     }
-
     public int pop() {
         if (isEmpty()) {
             System.out.println("Cannot pop: Stack is empty");
@@ -18,7 +14,6 @@ public class StackUsingArrayList {
         }
         return elements.remove(elements.size() - 1);
     }
-
     public int peek() {
         if (isEmpty()) {
             System.out.println("Stack is empty");
@@ -26,29 +21,22 @@ public class StackUsingArrayList {
         }
         return elements.get(elements.size() - 1);
     }
-
     public boolean isEmpty() {
         return elements.isEmpty();
     }
-
     public void printStack() {
         System.out.println("Stack: " + elements);
     }
-
     public static void main(String[] args) {
         StackUsingArrayList stack = new StackUsingArrayList();
         stack.push(5);
         stack.push(15);
         stack.push(25);
-
         stack.printStack();
-
         System.out.println("Top: " + stack.peek());
-
         System.out.println("Popped: " + stack.pop());
-
         stack.printStack();
-
         System.out.println("Is empty? " + stack.isEmpty());
     }
 }
+

76c67ee33172729290bf52da01695bff06e9ba3d
asifshaik4517-MI <asifshaik4517@gmail.com>
2025-08-19 14:36:37 +0530

    Update binary.java

--- binary.java
+++ binary.java
@@ -4,8 +4,6 @@ class binary
     {
         while (l <= r) {
             int m = (l + r) / 2;
-
-            // Index of Element Returned
             if (a[m] == x) {
                 return m;
             } else if (a[m] > x) {
@@ -21,11 +19,8 @@ class binary
         int a[] = { 2, 3, 4, 10, 40,50,60,80,20,50,31 };
         int n = a.length;
         int x = 10;
-
         int res = binarySearch(a, 0, n - 1, x);
-
         System.out.println("Element to be searched is : "+ x);
-
         if (res == -1)
             System.out.println("Element is not present in array");
         else
